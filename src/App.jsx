f29c664a3f96948c287331d677137091f6930964
Gulay <gulaymovlamova@gmail.com>
2024-12-26 18:59:30 +0400

    foundation of the project

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './assets/common/base.scss'
+
+function App() {
+  
+  return (
+    <div>
+      <h1>Course Project</h1>
+    </div>
+  )
+}
+
+export default App
---
+++ src/router/UserRouter.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+
+const UserRouter = () => {
+  return (
+  )
+}
+
+export default UserRouter
