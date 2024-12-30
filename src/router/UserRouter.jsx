75b90b78acaefad902cc7bccffe2b90624ccb7cb
Gulay <gulaymovlamova@gmail.com>
2024-12-30 11:44:30 +0400

    changes

--- src/router/UserRouter.jsx
+++ src/router/UserRouter.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import { Route, Routes } from 'react-router-dom'
 import UserLayout from '../layouts/user/UserLayout'
+import HomeBottom from '../pages/user/HomePage/HomeBottom/HomeBottom'
 
 const UserRouter = () => {
   return (
     <Routes>
       <Route element={<UserLayout />}>
-        <Route index path='/' element={<h1>Home</h1>} />
+        <Route index path='/' element={<HomeBottom />} />
         <Route path='/about' element={<h1>About</h1>} />
         <Route path='/education' element={<h1>Tedris saheleri</h1>} />
         <Route path='/students' element={<h1>Mezunlarimiz</h1>} />
