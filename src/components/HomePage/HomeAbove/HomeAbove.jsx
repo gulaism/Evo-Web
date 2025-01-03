69219e56bc0c0294f3e843f9e3e661fd29110ad7
Gulay <gulaymovlamova@gmail.com>
2025-01-03 18:29:42 +0400

    added: relation between button in welcome component with education route

--- src/components/HomePage/HomeAbove/HomeAbove.jsx
+++ src/components/HomePage/HomeAbove/HomeAbove.jsx
@@ -6,6 +6,7 @@ import rightArrow from "../../../assets/images/HomePage/Expand_right.svg";
 import plus from "../../../assets/images/HomePage/plus.svg";
 import cursorIcon from "../../../assets/images/HomePage/Vector.svg";
 import minus from "../../../assets/images/HomePage/minus.svg";
+import { Link } from "react-router-dom";
 
 const eduFields = [
   {
@@ -319,7 +320,7 @@ const HomeAbove = () => {
           iş dünyasında uğur qazanmaq üçün lazım olan bilik və təcrübəni bu gün
           əldə edin.
         </div>
-        <button>Kəşf et</button>
+        <button><Link style={{cursor: "none"}} to="/education">Kəşf et</Link></button>
       </div>
 
       <div className={styles.eduFields}>
