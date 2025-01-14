424c35454c4163cea9464a9fc46a41280b889452
Nasiba1719 <salmanli.91@mail.ru>
2025-01-14 17:38:15 +0400

    aboutUsStructureDone

---
+++ src/pages/user/AboutUs/AboutUs.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import AboutUsAbove from '../../../components/AboutUs/AboutUsAbove/AboutUsAbove'
+import AboutUsBottom from '../../../components/AboutUs/AboutUsBottom/AboutUsBottom'
+
+
+const AboutUs = () => {
+    return (
+        <div>
+            <AboutUsAbove />
+            <div style={{ marginBottom: "150px" }}></div>
+            <AboutUsBottom />
+        </div>
+    );
+}
+
+export default AboutUs
