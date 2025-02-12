e877dffaff469ef83452541101a7a26540d57934
Gulay <gulaymovlamova@gmail.com>
2025-02-12 18:55:43 +0400

    added: partners to main dashboard

--- src/components/admin/MainDashBoard/Partners/Partners.jsx
+++ src/components/admin/MainDashBoard/Partners/Partners.jsx
@@ -1,14 +1,89 @@
-import React from "react";
+// style
 import styles from "./Partners.module.scss";
+// icons
+import editIcon from "../../../../assets/images/admin/Statistics/editIcon.svg";
+import partnerIcon from "../../../../assets/images/admin/Partners/partnerIcon.svg";
+import trashIcon from "../../../../assets/images/admin/Partners/trashIcon.svg";
+import plusIcon from "../../../../assets/images/admin/Partners/plusIcon.svg";
+
 
 const Partners = () => {
   return (
     <div className={styles.partners}>
-      <h2>Tərəfdaşlarımız</h2>
-      <div className={styles.partnerLogos}>
-        <img src="/images/partner1.png" alt="Partner 1" />
-        <img src="/images/partner2.png" alt="Partner 2" />
-        <img src="/images/partner3.png" alt="Partner 3" />
+      <h2>Partnyorlar</h2>
+      <div className={styles.title}>Partnyorlar</div>
+      <div className={styles.partnerList}>
+        <div>
+          <div className={styles.editIcon}>
+            <img src={editIcon} alt="" />
+          </div>
+          <div className={styles.partnerIcon}>
+            <img src={partnerIcon} alt="" />
+          </div>
+          <div className={styles.trashIcon}>
+            <img src={trashIcon} alt="" />
+          </div>
+        </div>
+        <div>
+          <div className={styles.editIcon}>
+            <img src={editIcon} alt="" />
+          </div>
+          <div className={styles.partnerIcon}>
+            <img src={partnerIcon} alt="" />
+          </div>
+          <div className={styles.trashIcon}>
+            <img src={trashIcon} alt="" />
+          </div>
+        </div>
+        <div>
+          <div className={styles.editIcon}>
+            <img src={editIcon} alt="" />
+          </div>
+          <div className={styles.partnerIcon}>
+            <img src={partnerIcon} alt="" />
+          </div>
+          <div className={styles.trashIcon}>
+            <img src={trashIcon} alt="" />
+          </div>
+        </div>
+        <div>
+          <div className={styles.editIcon}>
+            <img src={editIcon} alt="" />
+          </div>
+          <div className={styles.partnerIcon}>
+            <img src={partnerIcon} alt="" />
+          </div>
+          <div className={styles.trashIcon}>
+            <img src={trashIcon} alt="" />
+          </div>
+        </div>
+        <div>
+          <div className={styles.editIcon}>
+            <img src={editIcon} alt="" />
+          </div>
+          <div className={styles.partnerIcon}>
+            <img src={partnerIcon} alt="" />
+          </div>
+          <div className={styles.trashIcon}>
+            <img src={trashIcon} alt="" />
+          </div>
+        </div>
+        <div>
+          <div className={styles.editIcon}>
+            <img src={editIcon} alt="" />
+          </div>
+          <div className={styles.partnerIcon}>
+            <img src={partnerIcon} alt="" />
+          </div>
+          <div className={styles.trashIcon}>
+            <img src={trashIcon} alt="" />
+          </div>
+        </div>
+      </div>
+      <div>
+        <div>
+          <img src={plusIcon} alt="" />
+        </div>
       </div>
     </div>
   );
--- src/pages/admin/Dashboard/Dashboard.jsx
+++ src/pages/admin/Dashboard/Dashboard.jsx
@@ -1,14 +1,23 @@
-// import Statistics from "../../../components/admin/Statistics/Statistics"
-
+// components
+import Partners from "../../../components/admin/MainDashBoard/Partners/Partners"
 import Statistics from "../../../components/admin/MainDashBoard/Statistics/Statistics"
+// style
+import styles from "./Dashboard.module.scss";
 
 
 const Dashboard = () => {
   return (
-    <div>
-      <Statistics />
+    <div className={styles.dashCont}>
+      <div className={styles.firstSection}>
+        <div>
+          <Statistics />
+        </div>
+        <div>
+          <Partners />
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
 export default Dashboard
