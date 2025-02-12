d93828c730a3a5ea9ead5cd00834dd86c4fd83dd
Gulay <gulaymovlamova@gmail.com>
2025-02-12 17:45:32 +0400

    added: statistics to the main dashboard

---
+++ src/components/admin/MainDashBoard/Statistics/Statistics.jsx
@@ -0,0 +1,76 @@
+import React from "react";
+import styles from "./Statistics.module.scss";
+import editIcon from "../../../../assets/images/admin/Statistics/editIcon.svg";
+
+const Statistics = () => {
+  return (
+    <div className={styles.statistics}>
+      <div className={styles.title}>Herobody</div>
+      <div className={styles.sections}>
+        <section>
+          <div className={styles.sectionTitle}>Statistika</div>
+          <div className={styles.inputCont}>
+            <div className={styles.iconCont}>
+              <img src={editIcon} alt="" />
+            </div>
+            <input type="number" placeholder="XX+ Məzun sayı" />
+            <div className={styles.ppCont}>
+              <div>
+                <img src="https://thispersondoesnotexist.com/" alt="" />
+              </div>
+              <div>
+                <img src="https://thispersondoesnotexist.com/" alt="" />
+              </div>
+              <div>
+                <img src="https://thispersondoesnotexist.com/" alt="" />
+              </div>
+            </div>
+          </div>
+          <div className={styles.inputCont}>
+            <div className={styles.iconCont}>
+              <img src={editIcon} alt="" />
+            </div>
+            <input type="number" placeholder="XX+ Pedaqoji Kollektiv" />
+            <div className={styles.ppCont}>
+              <div>
+                <img src="https://thispersondoesnotexist.com/" alt="" />
+              </div>
+              <div>
+                <img src="https://thispersondoesnotexist.com/" alt="" />
+              </div>
+              <div>
+                <img src="https://thispersondoesnotexist.com/" alt="" />
+              </div>
+            </div>
+          </div>
+        </section>
+        <section>
+          <div className={styles.sectionTitle}>Sloqan</div>
+          <div className={styles.inputCont}>
+            <div className={styles.iconCont}>
+              <img src={editIcon} alt="" />
+            </div>
+            <input
+              type="text"
+              placeholder="Gələcəyin bacarıqları bu günün akademiyasında!"
+            />
+          </div>
+        </section>
+        <section>
+          <div className={styles.sectionTitle}>Təsvir</div>
+          <div className={styles.inputCont}>
+            <div className={styles.iconCont}>
+              <img src={editIcon} alt="" />
+            </div>
+            <input
+              type="text"
+              placeholder="Texnologiya ilə uyğunlaşın, bacarıqlarınızı inkişaf etdirin, gələcəyin iş dünyasında uğur qazanmaq üçün lazım olan bilik və təcrübəni bu gün əldə edin."
+            />
+          </div>
+        </section>
+      </div>
+    </div>
+  );
+};
+
+export default Statistics;
--- src/pages/admin/Dashboard/Dashboard.jsx
+++ src/pages/admin/Dashboard/Dashboard.jsx
@@ -1,19 +1,12 @@
-import React from 'react'
-import Statistics from '../../../components/admin/Statistics/Statistics'
-import Partners from '../../../components/admin/Partners/Partners'
-import Courses from '../../../components/admin/Courses/Courses'
-import Reviews from '../../../components/admin/Reviews/Reviews'
+// import Statistics from "../../../components/admin/Statistics/Statistics"
+
+import Statistics from "../../../components/admin/MainDashBoard/Statistics/Statistics"
+
 
 const Dashboard = () => {
   return (
     <div>
-        <Statistics />
-        <Reviews /> 
-        <Partners />
-        <Courses />
-        <Courses />
-        <Courses />
-        <Courses />
+      <Statistics />
     </div>
   )
 }
