c015eeb535353b96ce062919198ce4a30a8c1af7
Gulay <gulaymovlamova@gmail.com>
2025-03-08 09:53:08 +0400

    added next group times to newcourse admin

--- src/components/admin/NewCoursePage/NextGroupTime/NextGroupTime.jsx
+++ src/components/admin/NewCoursePage/NextGroupTime/NextGroupTime.jsx
@@ -1,12 +1,98 @@
 // styles
 import styles from './NextGroupTime.module.scss';
+// icons
+import edit from "../../../../assets/images/admin/Statistics/editIcon.svg";
 
 const NextGroupTime = () => {
   return (
-    <div>
-      group time
+    <div className={styles.nextGroupTimeCont}>
+      <div className={styles.ngtTit}>Növbəti Qruplar</div>
+      <div className={styles.ngtBody}>
+        <div className={styles.ngtWeekly}>
+          <div className={styles.weeklyTop}>
+            <div className={styles.topLeft}>Həftəiçi</div>
+            <div className={styles.topRight}>
+              <div className={styles.rightWord}>var</div>
+              <div className={styles.rightCircle}></div>
+            </div>
+          </div>
+          <div className={styles.weeklyBot}>
+            <div className={styles.botIc}>
+              <img src={edit} alt="" />
+            </div>
+            <input
+              className={styles.botInput}
+              type="text"
+              placeholder="Tarix"
+            />
+          </div>
+          <div className={styles.weeklyBotBottom}>
+            <div className={styles.weeklyBot}>
+              <div className={styles.botIc}>
+                <img src={edit} alt="" />
+              </div>
+              <input
+                className={styles.botInput}
+                type="text"
+                placeholder="Saat"
+              />
+            </div>
+            <div className={styles.weeklyBot}>
+              <div className={styles.botIc}>
+                <img src={edit} alt="" />
+              </div>
+              <input
+                className={styles.botInput}
+                type="text"
+                placeholder="Saat"
+              />
+            </div>
+          </div>
+        </div>
+        <div className={styles.ngtWeekly}>
+          <div className={styles.weeklyTop}>
+            <div className={styles.topLeft}>Həftəsonu</div>
+            <div className={styles.topRight}>
+              <div className={styles.rightWord}>var</div>
+              <div className={styles.rightCircle}></div>
+            </div>
+          </div>
+          <div className={styles.weeklyBot}>
+            <div className={styles.botIc}>
+              <img src={edit} alt="" />
+            </div>
+            <input
+              className={styles.botInput}
+              type="text"
+              placeholder="Tarix"
+            />
+          </div>
+          <div className={styles.weeklyBotBottom}>
+            <div className={styles.weeklyBot}>
+              <div className={styles.botIc}>
+                <img src={edit} alt="" />
+              </div>
+              <input
+                className={styles.botInput}
+                type="text"
+                placeholder="Saat"
+              />
+            </div>
+            <div className={styles.weeklyBot}>
+              <div className={styles.botIc}>
+                <img src={edit} alt="" />
+              </div>
+              <input
+                className={styles.botInput}
+                type="text"
+                placeholder="Saat"
+              />
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
 export default NextGroupTime
