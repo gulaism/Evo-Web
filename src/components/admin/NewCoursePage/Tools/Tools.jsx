93e851ce12677e6388330b3bde4d2401c43c6a45
Gulay <gulaymovlamova@gmail.com>
2025-03-08 06:57:38 +0400

    tools section added to the newcourse admin

--- src/components/admin/NewCoursePage/Tools/Tools.jsx
+++ src/components/admin/NewCoursePage/Tools/Tools.jsx
@@ -1,11 +1,89 @@
-import React from 'react'
+// icons
+import plus from '../../../../assets/images/admin/Partners/plusIcon.svg';
+import trash from '../../../../assets/images/admin/Partners/trashIcon.svg';
+// styles
+import styles from './Tools.module.scss';
 
 const Tools = () => {
   return (
-    <div>
-      hello
+    <div className={styles.toolsSec}>
+      <div className={styles.toolTit}>Tədris proqramı</div>
+      <div className={styles.toolHead}>
+        <div>Hansı alətlərlə işləyəcəksən?</div>
+        <div className={styles.headIc}>
+          <img src={plus} alt="" />
+        </div>
+      </div>
+      <div className={styles.toolsGrid}>
+        <div className={styles.oneTool}>
+          <div className={styles.toolLeft}>
+            <div className={styles.toolIcOne}>
+              <img src={plus} alt="" />
+            </div>
+            <input className={styles.toolInput} type="text" placeholder="Alət" />
+          </div>
+          <div className={styles.toolIcTwo}>
+            <img src={trash} alt="" />
+          </div>
+        </div>
+        <div className={styles.oneTool}>
+          <div className={styles.toolLeft}>
+            <div className={styles.toolIcOne}>
+              <img src={plus} alt="" />
+            </div>
+            <input className={styles.toolInput} type="text" placeholder="Alət" />
+          </div>
+          <div className={styles.toolIcTwo}>
+            <img src={trash} alt="" />
+          </div>
+        </div>
+        <div className={styles.oneTool}>
+          <div className={styles.toolLeft}>
+            <div className={styles.toolIcOne}>
+              <img src={plus} alt="" />
+            </div>
+            <input className={styles.toolInput} type="text" placeholder="Alət" />
+          </div>
+          <div className={styles.toolIcTwo}>
+            <img src={trash} alt="" />
+          </div>
+        </div>
+        <div className={styles.oneTool}>
+          <div className={styles.toolLeft}>
+            <div className={styles.toolIcOne}>
+              <img src={plus} alt="" />
+            </div>
+            <input className={styles.toolInput} type="text" placeholder="Alət" />
+          </div>
+          <div className={styles.toolIcTwo}>
+            <img src={trash} alt="" />
+          </div>
+        </div>
+        <div className={styles.oneTool}>
+          <div className={styles.toolLeft}>
+            <div className={styles.toolIcOne}>
+              <img src={plus} alt="" />
+            </div>
+            <input className={styles.toolInput} type="text" placeholder="Alət" />
+          </div>
+          <div className={styles.toolIcTwo}>
+            <img src={trash} alt="" />
+          </div>
+        </div>
+        <div className={styles.oneTool}>
+          <div className={styles.toolLeft}>
+            <div className={styles.toolIcOne}>
+              <img src={plus} alt="" />
+            </div>
+            <input className={styles.toolInput} type="text" placeholder="Alət" />
+          </div>
+          <div className={styles.toolIcTwo}>
+            <img src={trash} alt="" />
+          </div>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
 export default Tools
